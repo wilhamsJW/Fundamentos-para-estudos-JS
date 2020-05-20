0b824955e35b028ba470f5cc58a78458304f7750
wilhamsJW <wilhamsmeira@gmail.com>
2020-05-20 19:06:52 -0300

    arrays and map,for in, filter, every, some

--- exemplos/Arrays.js
+++ exemplos/Arrays.js
@@ -146,7 +146,14 @@ console.log(result);
 */
 
 
+
+
+
 //                         sort and reverse         alinhando itens
+
+
+
+
 
 /*
 let list = ['email', 'telefone', 'endereço', 'nome'] ;
@@ -157,4 +164,140 @@ list.sort().reverse();  //sort coloca os itens em ordem alfabética // reverse i
 let result = list.toString();
                             
 console.log(result); //telefone,nome,endereço,email / o sort colocou em ordem alfabética e o reverse reverte a ordem alfabética
+*/
+
+
+
+
+//                                              map
+
+
+/*
+let list = [45, 4, 9, 16, 25] ;
+let list2 = []; //criei uma nova array para mostrar ela com os números multiplicados na let list2
+
+list2 = list.map(function(item){  //map percorre todos os itens da array e executa o código que se pede
+
+    return item * 2; //multiplique os némros da minha aray
+
+})
+
+let result = list2;
+
+console.log(result); //[90, 8, 18, 32, 50]
+*/
+
+
+// Também podemos fazer isso com um for in
+
+
+/*
+let list = [45, 4, 9, 16, 25] ;
+let list2 = []; //criei uma nova array para mostrar ela com os números multiplicados na let list2
+
+for (let i in list) {  //for in, percorre todos os campos da minha array executando o código que se segue
+
+    list2.push(list[i] * 2 );
+
+}
+
+let result = list2;
+
+console.log(result); //[90, 8, 18, 32, 50]
+*/
+
+
+
+
+//                                      filter();
+
+
+
+/*
+let list = [45, 4 , 9 , 16 , 25] ;
+let list2 = [];
+
+list2 = list.filter(function(item){  //filter só permitiu passar quem fosse menor q 20
+
+    if (item < 20) {
+        return true;
+    } else {
+        return false;
+    }
+
+});
+
+let result = list2;
+                            
+console.log(result); 
+*/
+
+
+
+
+//                                     every();
+
+
+/*
+let list = [45, 4 , 9 , 16 , 25] ;
+let list2 = [];
+
+list2 = list.every(function(item){  //every = todos, se todos forem maior q 20 ele retorna true, se não é false
+                                   //every avalia cada item e executa a ação lhe dada
+                                  //todos tem que ter a execução da condição que lhe foi pedida
+    if(item > 20){  
+        return true;
+    } else {
+        return false;
+    }
+
+});
+
+let result = list2;
+                            
+console.log(result); //false
+*/
+
+
+
+//                                    some();
+
+
+/*
+let list = [45, 4 , 9 , 16 , 25] ;
+let list2 = [];
+
+list2 = list.some(function(item){  //some = alguns, ele retorna true pq alguns números são maiores q 20
+                                  //ele executa a primeira condição se ele achar pelo menos um item que conrreponda 
+                                 //á primeira condição   
+    if(item > 20){  
+        return true;
+    } else {
+        return false;
+    }
+
+});
+
+let result = list2;
+                            
+console.log(result); //true pq 25 é maior q 20
+*/
+
+
+// código usando every ou some só que de forma reduzida   usando if ternário
+
+/*
+let list = [45, 4 , 9 , 16 , 25] ;
+let list2 = [];
+
+list2 = list.every(function(item){    
+ 
+        return(item > 20) ? true : false; // este é p if ternário, um atalho pra o if e else em alguns casos
+                                         // os dois pontos -> :  , são o equivalente a ou, true ou false, também é if e o else 
+
+});
+
+let result = list2;
+                            
+console.log(result); 
 */
